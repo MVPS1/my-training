5139d50419fff8b23d6f4447414d334b23a458f3
Osama <121611596+MVPS1@users.noreply.github.com>
2024-06-25 08:43:18 +0000

    second com

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 print(app)
 print("getfff")
 
-
+JOBS = ["name", "game", "shame", "tame", "fame"]
+TITLE = "OSAMA's page"
 @app.route('/')
 def hello_world():
-  return 'Hello, HHggHH!'
+  return render_template('temp.html', names=JOBS, title=TITLE)
 
-
+@app.route('/jobs')
+def list_jobs():
+  return jsonify(JOBS)
+    
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True, port=8080)
